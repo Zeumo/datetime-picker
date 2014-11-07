a85a344bbc9f6c942770b90d23804b771fdd5214
Nic Aitch <nic@nicinabox.com>
2014-11-07 13:52:57 -0600

    Update example

--- example/main.js
+++ example/main.js
@@ -2,10 +2,11 @@ $(function() {
   $('.date-range-picker input').picker();
 
   $('.button-picker a').picker({
-    outputTo: $('.button-picker input')
+    outputTo: $('.button-picker input'),
+    prefill: true
   });
 
-  $('.prefill-picker input').picker();
+  $('.prefilled-picker input').picker();
 
   $('.button-picker a').on('datepicker.remove', function(e, el) {
     $(el).val('');
